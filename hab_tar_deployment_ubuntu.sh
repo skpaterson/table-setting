07ca8441dd5fa52fed7376544f64be0850ae6404
Stuart Paterson <spaterson@chef.io>
2018-08-16 15:12:10 +0100

    Removing unnecessary hab pkg install command from sample ubuntu installation script.
    Signed-off-by: Stuart Paterson <spaterson@chef.io>

--- hab_tar_deployment_ubuntu.sh
+++ hab_tar_deployment_ubuntu.sh
@@ -7,6 +7,5 @@ sudo tar zxvf *-table-setting*.tar.gz -C /
 # run app in SQLite file mode
 export DB_TYPE=sqlite_file
 # install and run with the habitat supervisor
-sudo /hab/bin/hab pkg install habskp/table-setting
 sudo /hab/bin/hab sup run &
 sudo /hab/bin/hab svc load habskp/table-setting
